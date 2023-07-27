b3147697aa77c5db0e1adb0371528352ee8dc468
Ali <aristanovali618@gmail.com>
2023-07-27 12:29:24 +0500

    little change in sql query

--- expense-management/internal/repository/dbrepo/postgres.go
+++ expense-management/internal/repository/dbrepo/postgres.go
@@ -3,12 +3,16 @@ package dbrepo
 import (
 	"github.com/nanmenkaimak/expense-management/internal/models"
 	"github.com/pkg/errors"
+	"time"
 )
 
 func (m *postgresDBRepo) CreateExpense(newExpense models.Expenses) (string, error) {
 	var expenseID string
+	if newExpense.Date.IsZero() {
+		newExpense.Date = time.Now()
+	}
 	err := m.DB.Get(&expenseID,
-		`insert into expenses (amount, category_id, user_id, description, date)
+		`insert into expenses (amount, category_id, user_id, description, date_time)
 				values ($1, $2, $3, $4, $5) returning id`,
 		newExpense.Amount, newExpense.CategoryID, newExpense.UserID, newExpense.Description, newExpense.Date)
 	if err != nil {
