97e473160773f9ea4a883a14da999de3ca684529
Ali <aristanovali618@gmail.com>
2023-07-28 20:31:06 +0500

    validation in sign up

--- user-management/go.mod
+++ user-management/go.mod
@@ -13,3 +13,5 @@ require (
 )
 
 require github.com/rabbitmq/amqp091-go v1.8.1
+
+require github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
--- user-management/internal/handlers/signup.go
+++ user-management/internal/handlers/signup.go
@@ -2,8 +2,12 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/asaskevich/govalidator"
 	"github.com/nanmenkaimak/user-management/internal/models"
+	"github.com/pkg/errors"
 	"net/http"
+	"unicode"
 )
 
 func (m *Repository) SignUp(w http.ResponseWriter, r *http.Request) {
@@ -16,10 +20,39 @@ func (m *Repository) SignUp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, err := m.DB.CreateUser(newUser)
+	ok := govalidator.IsEmail(newUser.Email)
+	if !ok {
+		http.Error(w, errors.New("invalid email").Error(), http.StatusBadRequest)
+		return
+	}
+	err := validPassword(newUser.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	_, err = m.DB.CreateUser(newUser)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	renderJSON(w, newUser)
+}
+
+func validPassword(s string) error {
+next:
+	for name, classes := range map[string][]*unicode.RangeTable{
+		"upper case": {unicode.Upper, unicode.Title},
+		"lower case": {unicode.Lower},
+		"numeric":    {unicode.Number, unicode.Digit},
+		"special":    {unicode.Space, unicode.Symbol, unicode.Punct, unicode.Mark},
+	} {
+		for _, r := range s {
+			if unicode.IsOneOf(classes, r) {
+				continue next
+			}
+		}
+		return fmt.Errorf("password must have at least one %s character", name)
+	}
+	return nil
 }
